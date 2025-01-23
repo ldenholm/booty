5655dcb2fc047c05c90a033f080b2f6f7eebdac0
lochie <disrute@gmail.com>
2025-01-23 21:35:35 +1100

    todo impl axum router

--- alfheim/src/main.rs
+++ alfheim/src/main.rs
@@ -1,12 +1,22 @@
 mod comic;
-use std::collections::HashMap;
+use axum::{
+    extract::{Path, Json},
+    routing::{get, post},
+    Router,
+};
+use std::{collections::HashMap, hash::Hash};
 use std::sync::{Arc, Mutex};
 use std::sync::RwLock;
 use std::net::SocketAddr;
 
 type Cache = Arc<RwLock<HashMap<String, comic::Comic>>>;
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    let comic_cache: Cache = Arc::new(RwLock::new(HashMap::new()));
+    
+    let app = Router::new();
+    
+    
     println!("Hello, world!");
-
 }
