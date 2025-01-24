cba7d0e8cfb5bf74cc046a05ff3ebe60f8bb4131
lochie <disrute@gmail.com>
2025-01-24 13:41:54 +1100

    syntax tricks

--- ints/src/main.rs
+++ ints/src/main.rs
@@ -6,4 +6,23 @@ fn main() {
     let c = Rc::new(Box::new(3)); // boxed integer wrapped within reference counter 
     let d = Arc::new(Mutex::new(4)); // int wrapped in atomic rc with mutual exc lock
     println!("a: {:?}, b: {:?}, c: {:?}, d: {:?}", a, b, c, d);
+    int_things();
+}
+
+
+fn int_things() {
+    let a = 10; // type inferred by compiler
+    let b: i32 = 20; // explicit type
+    let c = 30i32; // include type annotation in its literal form
+    let d = 30_i32; // identical value to c, underscore has no functional impact 
+    let e = add(a, b) + add(c, d);
+    println!("(a + b) + (c + d) = {}. Associativity of addition.", e);
+}
+
+// must declare types when constructing functions
+fn add(x: i32, y: i32) -> i32 {
+    // return is implicit
+    x + y
+    // note x + y; with semicolon produces return type of unit: ()
+    // rather than i32.
 }
