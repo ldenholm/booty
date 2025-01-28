5dd79874dbdb54d917c0da36d9e87b79f75257d8
lochie <disrute@gmail.com>
2025-01-28 14:37:59 +1100

    mutating collection elements

--- flow/src/main.rs
+++ flow/src/main.rs
@@ -1,7 +1,9 @@
-use std::collections::btree_map::Range;
+use std::ops::MulAssign;
+
 
 fn main() {
     let mut myarr: Vec<i8> = vec![];
+    let mut t: Vec<i8> = vec![];
     /* myarr[0] = 5;
        myarr[1] = 10;
        this is wrong we need to push
@@ -12,10 +14,39 @@ fn main() {
         myarr.push(n * 2);
     }
 
+    t.push(1);
+    t.push(2);
+    t.push(3);
+
+    for x in &t {
+        // do something with x
+        print!("{x}\n");
+    }
+
+    // now container t should not be accessible twice
+
+    for x in &mut t {
+        println!("We may access container t twice when using a reference.");
+        //x *= 2; // illegal since we don't use mut reference.
+
+        // by invoking &mut t we can now mutate our collection elements:
+        x.mul_assign(2);
+    }
+
+    for x in 0..t.len() {
+        println!("value of idx: {}", x);
+        println!("{}", t[x]);
+        println!("\n");
+    }
+
+    // Above block fails to compile. We can change the first
+    // for loop to use a reference to the container &t allowing
+    // us to loop over the container n times.
+
     println!("length of myarr = {}", myarr.len());
 
     for x in myarr {
-        print!("{}", x);
+        print!("{x}");
         print!("\n");
     }
 }
