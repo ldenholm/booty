482560b1f531a853341e0a3a6bbc8c13a09383b1
lochie <disrute@gmail.com>
2025-02-08 12:36:10 +1100

    add deref notes, mandlebrot

--- referencing/src/main.rs
+++ referencing/src/main.rs
@@ -1,8 +1,10 @@
 fn main() {
-    let a = 42;
-    let r = &a;
+    let a = 42; // dec and assignment, a has val 42
+    let r = &a; // r is a ref to a, using '&' ref operator
     let sum = a + *r;
+    let deref = *r;
     println!("sum of a + a: {sum}");
+    println!("is dereferencing destructive?: {deref}. No.");
 
     let needle = 42;
     let haystack = [1, 1, 2, 5, 15, 42, 203, 877, 4140, 21147];
@@ -12,4 +14,22 @@ fn main() {
             println!("{item}");
         }
     }
+
+    for something in haystack {
+        println!("something is: {something}");
+    }
+
+    for another_thing in &haystack {
+        let test = *another_thing;
+        let cool = another_thing;
+        println!("anotherThing is: {another_thing}");
+        println!("test is: {test}");
+        println!("cool is: {cool}");
+    }
+
+    /*
+        let's talk about whats going on here
+
+
+     */
 }
